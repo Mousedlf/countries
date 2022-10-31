b49148ccd02640d502076d9c176b8ec66b85eb28
Mousedlf <dlfcaroline@gmail.com>
2022-10-31 20:51:36 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+async function getCountries(){
+    let response = await fetch('https://restcountries.com/v3.1/lang/french');
+    let data = await response.json();
+    console.log(data.length)
+
+    const getRandomCountryFromArray = Math.floor(Math.random()* data.length)
+    const randomCountryFromArray = data[getRandomCountryFromArray].name.common;
+    console.log(randomCountryFromArray)
+
+
+}
+getCountries()
+
+
