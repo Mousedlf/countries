71899a579ed0546f94f08e6b8f2d712b46f592a2
Mousedlf <dlfcaroline@gmail.com>
2022-11-01 14:17:56 +0100

    essai

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
+
+const buttons = document.querySelector('#buttons')
+
 async function getCountries(){
-    let response = await fetch('https://restcountries.com/v3.1/lang/french');
+    let response = await fetch('https://restcountries.com/v3.1/all');
     let data = await response.json();
-    console.log(data.length)
 
     const getRandomCountryFromArray = Math.floor(Math.random()* data.length)
     const randomCountryFromArray = data[getRandomCountryFromArray].name.common;
-    console.log(randomCountryFromArray)
+    console.log('random country : ' + randomCountryFromArray)
+
+    const randomCountry = document.querySelector('#country')
+    randomCountry.innerHTML += randomCountryFromArray
 
     const numberOfCountries = document.querySelector('#nb')
     let numberOfCountriesValue = data.length
 
     numberOfCountries.innerHTML += numberOfCountriesValue
+
 }
 getCountries()
+
+
+
+/*
+languages.forEach(language =>{
+    console.log(language)
+    let template = `<div id="${language}" class="btn border-dark">${language}</div>`
+    document.querySelector('#buttons').innerHTML+=template
+})
+*/
+
+
 
 
 
